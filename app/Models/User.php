a285dc28d86fd2793eba34f24532e0f5927f0534
Oyatillo2002 <xoyatillo921@gmail.com>
2026-09-04 01:40:52 +0500

    authcontroller yaratildi, sanctum install qilindi: token orqali auth qilish uchun, LoginRequest yaratildi

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -53,5 +54,10 @@ class User extends Authenticatable
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class);
+    }
+
+    public function favorites(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
     }
 }
