a927d55a30998a42dbe7c1ab85cc1d76020ec5f9
Oyatillo2002 <xoyatillo921@gmail.com>
2026-09-14 00:35:35 +0500

    Review model -a yaratildi

---
+++ app/Models/Review.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Review extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReviewFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'rating',
+        'body'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->BelongsTo(Product::class);
+    }
+}
